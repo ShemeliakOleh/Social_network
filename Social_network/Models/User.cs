49e66731569e23f1c3e70b3a2d7d91f7a7f50bc4
Oleh_Shemeliak <Shemeliak.Oleh.Main@gmail.com>
2020-10-17 13:12:13 +0300

    Created a Model,Controller. Connected MongoDB

---
+++ Social_network/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_network.Models
+{
+    class User
+    {
+        
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string[] Interests { get; set; }
+        public string[] Following { get; set; }
+        public string[] Followers { get; set; }
+        public string[] Posts { get; set; }
+        public string[] Comments { get; set; }
+
+    }
+}
